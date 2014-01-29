c18197b9367c9132924e06025d819656386a9ba0
Jeremy Wright <jeremy@codestrokes.com>
2014-01-28 21:36:19 -0700

    Solved 1.1

--- 1.3.py
+++ 1.3.py
@@ -4,9 +4,7 @@ message = "jxu auo veh rhuqaydw oekh dunj syfxuh yi hywxj kdtuh oekh deiu"
 
 alphas = [i for i in string.ascii_lowercase]
 for i in range(26):
-    first = alphas[i:]
-    second = alphas[:i]
-    key = first+second
+    key = alphas[i:]+alphas[:i]
     #print key
     t = string.maketrans(str(alphas), str(key))
     print "[{0}] {1}".format(i, message.translate(t))
