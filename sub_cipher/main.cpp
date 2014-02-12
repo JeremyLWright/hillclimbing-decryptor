21faecaba4c8b7ecc92f97d00b3748f1d9491474
Jeremy Wright <jeremy@codestrokes.com>
2014-02-12 08:56:55 -0700

    Fixed a bug that would build the wrong key.

--- sub_cipher/main.cpp
+++ sub_cipher/main.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <ostream>
 #include <map>
 #include <string>
 #include <fstream>
@@ -8,83 +9,41 @@
 #include <random>
 #include <locale>
 #include "ngram.hpp"
+#include <stdexcept>
 
 namespace pystring {
 
-    std::string translate( const std::string & str, const std::string & table, const std::string & deletechars ="")
+    std::string translate( const std::string & str, const std::string & table)
     {
-        std::string s;
-        std::string::size_type len = str.size(), dellen = deletechars.size();
+        std::string s(str);
+        std::string::size_type len = str.size();
 
         if ( table.size() != 256 )
         {
-            // TODO : raise exception instead
-            return str;
+            throw std::runtime_error("Improper table size. Size must be 256 chars");
         }
-
-        //if nothing is deleted, use faster code
-        if ( dellen == 0 )
-        {
-            s = str;
-            for ( std::string::size_type i = 0; i < len; ++i )
-            {
-                s[i] = table[ s[i] ];
-            }
-            return s;
-        }
-
-
-        int trans_table[256];
-        for ( int i = 0; i < 256; i++)
-        {
-            trans_table[i] = table[i];
-        }
-
-        for ( std::string::size_type i = 0; i < dellen; i++)
-        {
-            trans_table[(int) deletechars[i] ] = -1;
-        }
-
+        
         for ( std::string::size_type i = 0; i < len; ++i )
         {
-            if ( trans_table[ (int) str[i] ] != -1 )
-            {
-                s += table[ str[i] ];
-            }
+            s[i] = table[ s[i] ];
         }
-
         return s;
-
     }
 
-
-std::string maketrans(std::map<char, char> trans)
-{
-    char t1data[256];
-    for(int i=0; i<256; ++i) t1data[i] = (char)i;
-    
-    for(auto& c : trans)
+    std::string maketrans(std::string key)
     {
-        t1data[c.first] = c.second;
-    }
-    return std::string(t1data, 256);
-}
+        char t1data[256];
+        std::iota(std::begin(t1data), std::end(t1data), 0);
+        size_t i = 'A';
+        size_t d = 'a' - 'A';
 
-std::string maketrans(std::string key)
-{
-    
-    char t1data[256];
-    std::iota(std::begin(t1data), std::end(t1data), 0);
-    size_t i = 'A';
-    size_t d = 'a' - 'A';
-    auto k = std::begin(key);
-    for(; k != std::end(key); ++i, ++k)
-    {
-        t1data[i] = *k;
-        t1data[i+d] = std::tolower(*k);
+        for(auto k = std::begin(key); k != std::end(key); ++i, ++k)
+        {
+            t1data[i] = *k;
+            t1data[i+d] = std::tolower(*k);
+        }
+        return std::string(t1data, 256);
     }
-    return std::string(t1data, 256);
-}
 
 }
 
@@ -92,7 +51,17 @@ struct cipher {
     std::string key;
     double score;
     std::string plaintext;
+    friend std::ostream& operator<<(std::ostream& os, cipher const & c);
 };
+
+std::ostream& operator<<(std::ostream& os, cipher const & c)
+{
+    os << "score:" << c.score << '\n';
+    os << "\tkey: " << c.key << '\n';
+    os << "\tplaintext: " << c.plaintext << '\n';
+    return os;
+}
+
 std::random_device rd;
 std::mt19937 g(rd());
 std::ifstream fin("../quadgrams.txt");
@@ -146,48 +115,8 @@ int main(int argc, const char *argv[])
         if(c.score > best.score)
         {
             best = c;
-            std::cout << "best score so far: " << best.score << " on iteration " << i << std::endl;
-            std::cout << "\tbest key: " << best.key << std::endl;
-            std::cout << "\tplaintext: " << best.plaintext << std::endl;
+            std::cout << c;
         }
     }
-#if 0
-    std::mt19937 g(rd());
-    std::string key("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-    std::shuffle(std::begin(key), std::end(key), g);
-    std::cout << pystring::maketrans(key);
-
-    std::map<char, char> m = {{'A', 'A'},
-        {'B', 'B'},
-        {'C', 'C'},
-        {'D', 'D'},
-        {'E', 'E'},
-        {'F', 'F'},
-        {'G', 'G'},
-        {'H', 'H'},
-        {'I', 'I'},
-        {'J', 'J'},
-        {'K', 'K'},
-        {'L', 'L'},
-        {'M', 'M'},
-        {'N', 'N'},
-        {'O', 'O'},
-        {'P', 'P'},
-        {'Q', 'Q'},
-        {'R', 'R'},
-        {'S', 'S'},
-        {'T', 'T'},
-        {'U', 'U'},
-        {'V', 'V'},
-        {'W', 'W'},
-        {'X', 'X'},
-        {'Y', 'Y'},
-        {'Z', 'Z'},
-    };
-    std::shuffle(std::begin(m), std::end(m), g);
-    auto t1 = pystring::maketrans(m);
-    std::string s1 = pystring::translate(cipher_text, t1);
-    std::cout << s1 <<  std::endl;
-#endif
     return 0;
 }
